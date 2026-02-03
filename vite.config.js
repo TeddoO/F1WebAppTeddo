434df8fd72a62562aa541460ddb39357996aea9c
Vinicius Toledo <206714326+TeddoO@users.noreply.github.com>
2026-02-03 12:03:02 +0000

    routing

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: '/F1WebAppTeddo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
